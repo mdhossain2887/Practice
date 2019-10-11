1795acce0352f0ec586c0f8a6904f925c663c22e
mdhossain2887 <56325613+mdhossain2887@users.noreply.github.com>
2019-10-11 10:31:49 -0400

    Update HomePage.java

--- ExpressProject/src/pages/HomePage.java
+++ ExpressProject/src/pages/HomePage.java
@@ -7,6 +7,8 @@ public class HomePage {
 	WebDriver driver;
 	public HomePage(WebDriver driver) {
 		
+		
+		
 		this.driver=driver;
 	}
 	public void cl(){
@@ -19,3 +21,4 @@ public class HomePage {
 		Shared.MouseOverToNewArrivals(driver, driver.findElement(ObjectRepo.newarrivalsLinkText), driver.findElement(ObjectRepo.mensnewarrivalsxpath));
 	}
 }
+// this is the edited line
